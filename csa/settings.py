ad63d9d738e1d92192d3d0a93b7caf14df851a0e
AndreySivyakov <solgas80@gmail.com>
2020-03-12 16:27:42 -0600

    db connection reconfigured

--- csa/settings.py
+++ csa/settings.py
@@ -76,15 +76,15 @@ WSGI_APPLICATION = 'csa.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 """
+
 DATABASES = {
     'default': {
         'ENGINE': 'sql_server.pyodbc',
@@ -99,7 +99,7 @@ DATABASES = {
         },
     },
 }
-"""
+
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
